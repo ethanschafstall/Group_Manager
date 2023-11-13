599ee72a753ec633655e150828841a530b01cf66
ethanschafstall <ethans9999@gmail.com>
2023-11-13 15:42:00 +0100

    Updated js

--- Web/src/script.js
+++ Web/src/script.js
@@ -1,14 +1,15 @@
 function addName() {
-    const nameInputGroup = document.getElementById('names');
+    const names = document.getElementById('names');
     const input = document.createElement('input');
     input.type = 'text';
-    input.placeholder = `Name ${nameInputGroup.children.length+1}`;
+    input.placeholder = `Name ${names.children.length+1}`;
     input.name = 'name';
     input.style.width = 'calc(100% - 16px)';
     input.style.padding = '8px';
     input.style.marginBottom = '16px';
     input.style.boxSizing = 'border-box';
-    nameInputGroup.insertBefore(input, nameInputGroup.children[nameInputGroup.children.length]);
+    names.insertBefore(input, names.children[names.children.length]);
+    displayStudents();
 }
 function removeName(){
     const nameInputGroup = document.getElementById('names');
@@ -16,5 +17,15 @@ function removeName(){
     if(nameInputGroup.children.length > 1)
     {
         nameInputGroup.removeChild(nameInputGroup.children[nameInputGroup.children.length-1]);
+    }
+}
+function displayStudents()
+{
+    const names = document.getElementById('names');
+    document.getElementById("displayStudents").innerHTML = names.children[0].value;
+    for (let index = 0; index < nameInputGroup.children.length; index++) {
+        
+        const input = document.createElement('input');
+        document.getElementById("displayStudents").innerHTML = names.children[i].value;
     }
 }
