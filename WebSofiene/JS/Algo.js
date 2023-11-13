78005a9c4d51818c23f7b542f05dee9e572b3641
termimi <123395118+termimi@users.noreply.github.com>
2023-11-13 14:45:55 +0100

    réparation

--- WebSofiene/JS/Algo.js
+++ WebSofiene/JS/Algo.js
@@ -1,15 +1,33 @@
-
+let tableauDeNom =[];
+let i =0;
 function ajouterChampNom() {
     // Récupérer la valeur du champ de saisie
     let nom = document.getElementById("nom").value;
+    tableauDeNom.push(nom)
 
     // Créer un nouvel élément div pour afficher le nom ajouté
     let nouveauChampNom = document.createElement("div");
     nouveauChampNom.textContent = nom;
+    
 
     // Ajouter le nouvel élément à la zone des noms ajoutés
     document.getElementById("nomsAjoutes").appendChild(nouveauChampNom);
 
     // Effacer le champ de saisie
     document.getElementById("nom").value = "";
+    
+    return tableauDeNom
+}
+function creationIDNom(){
+    let tableauNom = ajouterChampNom();
+    
+    for(i;i < tableauNom.length; i++){
+        console.log(tableauNom[i]);     
+    }
+}
+function creationIDNom2(){
+    let tableauNom = ajouterChampNom();
+    
+   console.log(tableauNom[i]);
+   i++;
 }
