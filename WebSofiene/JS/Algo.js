45259bce6244c1104ba86d70f58342c312a5f343
termimi <123395118+termimi@users.noreply.github.com>
2023-11-20 14:18:03 +0100

    groupe simple oke

--- WebSofiene/JS/Algo.js
+++ WebSofiene/JS/Algo.js
@@ -1,30 +1,33 @@
 let tableauDeNom =[];
 let i =0;
-function ajouterChampNom() {
-    // Récupérer la valeur du champ de saisie
-    let nom = document.getElementById("nom").value;
-    tableauDeNom.push(nom);
+function ajouterChampNom(check = true) {
+    console.log(check);
+    if (check){
+        // Récupérer la valeur du champ de saisie
+        let nom = document.getElementById("nom").value;
+        tableauDeNom.push(nom);
 
-    // Créer un nouvel élément div pour afficher le nom ajouté
-    let nouveauChampNom = document.createElement("div");
-    nouveauChampNom.textContent = nom;
-    
+        // Créer un nouvel élément div pour afficher le nom ajouté
+        let nouveauChampNom = document.createElement("div");
+        nouveauChampNom.textContent = nom;
+        
 
-    // Ajouter le nouvel élément à la zone des noms ajoutés
-    document.getElementById("nomsAjoutes").appendChild(nouveauChampNom);
+        // Ajouter le nouvel élément à la zone des noms ajoutés
+        document.getElementById("nomsAjoutes").appendChild(nouveauChampNom);
 
-    // Effacer le champ de saisie
-    document.getElementById("nom").value = "";
-    
+        // Effacer le champ de saisie
+        document.getElementById("nom").value = "";
+    }
     return tableauDeNom;
 }
 function creationGroupe(){
-    let tableauNom = ajouterChampNom();
+    let tableauNom = ajouterChampNom(false);
+    let groupe =[];
     let randomId =0;
-    for(i =0;i < 4; i++){
+    for(i = 0;i < 2; i++){
         randomId = Math.floor(Math.random() * tableauNom.length);
-        console.log(tableauNom[randomId]); 
-        tableauNom.splice(randomId,1);
+        groupe.push(tableauNom[randomId]);
+        tableauNom.splice(randomId,1)
     }
-    
+    console.log(groupe);
 }
