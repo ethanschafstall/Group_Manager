a2b4f0643bc27d39d72d199a2422ff7152a056f7
ethanschafstall <ethans9999@gmail.com>
2023-11-20 15:39:00 +0100

    Updated js, css, html

--- Web/src/script.js
+++ Web/src/script.js
@@ -1,16 +1,15 @@
-function addName() {
-    const names = document.getElementById('names');
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.placeholder = `Name ${names.children.length+1}`;
-    input.name = 'name';
-    input.style.width = 'calc(100% - 16px)';
-    input.style.padding = '8px';
-    input.style.marginBottom = '16px';
-    input.style.boxSizing = 'border-box';
-    names.insertBefore(input, names.children[names.children.length]);
-    displayStudents();
-}
+var names=[];
+
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var code = event.code;
+    if (code == "Enter") {
+        addToList();
+        const InputBox = document.getElementById('name');  
+        InputBox.value = '';
+    }
+  }, false);
+
 function removeName(){
     const nameInputGroup = document.getElementById('names');
     const input = document.createElement('input'); 
@@ -19,13 +18,54 @@ function removeName(){
         nameInputGroup.removeChild(nameInputGroup.children[nameInputGroup.children.length-1]);
     }
 }
-function displayStudents()
-{
-    const names = document.getElementById('names');
-    document.getElementById("displayStudents").innerHTML = names.children[0].value;
-    for (let index = 0; index < nameInputGroup.children.length; index++) {
-        
-        const input = document.createElement('input');
-        document.getElementById("displayStudents").innerHTML = names.children[i].value;
+function addToList(){
+    boxvalue = document.getElementById('name').value;
+    if (boxvalue != "") {
+        names.push(boxvalue);  
+        console.log(names);
+    }
+    const InputBox = document.getElementById('name');  
+    InputBox.value = '';
+    removeAllChildNodes();
+    displayNameList();
+}
+function displayNameList(){
+    
+    const nameList = document.getElementById('nameList');
+
+    for (let i = 0; i < names.length; i++) {
+        const liElement = document.createElement('button');
+        liElement.textContent = names[i];
+        liElement.classList.add('name');
+        liElement.classList.add('button');
+        liElement.setAttribute('onclick', 'removeName(this.id)');
+        liElement.setAttribute('id', `${i}`,);
+        nameList.appendChild(liElement);
+    }
+}
+function removeAllChildNodes() {
+    const nameList = document.getElementById('nameList');
+    nameList.innerHTML = '';
+}
+function removeName(id){
+    console.log(id);
+    names.splice(id,1);
+    removeAllChildNodes();
+    displayNameList();
+}
+function createGroups(){
+    const groupSize = document.getElementById('groupSize');
+}
+// n being the amount of people, r being the group size.
+function amountOfWeeks(n, r){
+    let nValue = fractorial(n);
+    let rValue = fractorial(r);
+}
+function fractorial(n){
+    if (n === 0){
+        return 1;
+    }
+    else{
+        return n* fractorial(n-1);
     }
 }
