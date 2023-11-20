83c72e06709a89bdd8047bbcd7be8c3e5f6ea385
termimi <123395118+termimi@users.noreply.github.com>
2023-11-20 12:27:26 +0100

    added WeSofiene

--- WebSofiene/JS/Algo.js
+++ WebSofiene/JS/Algo.js
@@ -3,7 +3,7 @@ let i =0;
 function ajouterChampNom() {
     // Récupérer la valeur du champ de saisie
     let nom = document.getElementById("nom").value;
-    tableauDeNom.push(nom)
+    tableauDeNom.push(nom);
 
     // Créer un nouvel élément div pour afficher le nom ajouté
     let nouveauChampNom = document.createElement("div");
@@ -16,18 +16,15 @@ function ajouterChampNom() {
     // Effacer le champ de saisie
     document.getElementById("nom").value = "";
     
-    return tableauDeNom
+    return tableauDeNom;
 }
-function creationIDNom(){
+function creationGroupe(){
     let tableauNom = ajouterChampNom();
-    
-    for(i;i < tableauNom.length; i++){
-        console.log(tableauNom[i]);     
+    let randomId =0;
+    for(i =0;i < 4; i++){
+        randomId = Math.floor(Math.random() * tableauNom.length);
+        console.log(tableauNom[randomId]); 
+        tableauNom.splice(randomId,1);
     }
-}
-function creationIDNom2(){
-    let tableauNom = ajouterChampNom();
     
-   console.log(tableauNom[i]);
-   i++;
 }
