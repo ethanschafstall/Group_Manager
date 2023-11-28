eb4b335b2f1832645ec4a353602c0dda9c556854
ethanschafstall <ethans9999@gmail.com>
2023-11-28 08:54:49 +0100

    Removed code

--- Web/src/script.js
+++ Web/src/script.js
@@ -64,29 +64,3 @@ function saveGroupSize(size) {
     console.log(groupSize);
 }
 
-
-
-function getInputValue() {
-  output.innerHTML = "";
-  const values = input.value;
-  const total = sums.value;
-  let person = values.split(",");
-
-  let random = person.sort(() => Math.random() - 0.5);
-  console.log(random);
-  
-  function chunk(array, size) {
-    if (array.length <= size) {
-      return [array];
-    }
-  
-    return [array.slice(0, size), ...chunk(array.slice(size), size)];
-}
-  
-var team = chunk(random, total);
-  for (let i = 0; i < team.length; i++) {
-    output.innerHTML += `<p> Team-${i + 1}: ${team[i]} </p>`;
-  }
-}
-
-
